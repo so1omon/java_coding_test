15e5aa79fba4fb720173d3dd05bde2150e54b95e
so1omon <solmin3665@gmail.com>
2023-02-03 15:06:43 +0900

    test commit for mobile push

--- src/main/java/김솔민/Test.java
+++ src/main/java/김솔민/Test.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 class Test {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,232,12,459);
-        System.out.println("list = " + list);
-
-        list.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
-
-        System.out.println("list = " + list);
+//        List<Integer> list = Arrays.asList(1,2,3,232,12,459);
+//        System.out.println("list = " + list);
+//
+//        list.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o1 - o2;
+//            }
+//        });
+//
+//        System.out.println("list = " + list);
     }
 }
