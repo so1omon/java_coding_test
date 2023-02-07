2ddd2134ffab99223f784064a8d77b33518e02ea
sunga <tjddk1117@naver.com>
2023-02-08 01:11:20 +0900

    2월 7일

---
+++ src/main/java/김성아/skill_tree.java
@@ -0,0 +1,31 @@
+package 김성아;
+import java.util.*;
+public class skill_tree {public int solution(String skill, String[] skill_trees) {
+    int answer = 0;
+    boolean[]arr=new boolean[26];
+    for(int i=0;i<skill_trees.length;i++){
+        Queue<Character>que=new LinkedList<>();
+        for(int j=0;j<skill.length();j++){
+            char ch=skill.charAt(j);
+            arr[ch-65]=true;
+            que.add(ch);
+        }
+        int j;
+        for(j=0;j<skill_trees[i].length();j++){
+            char temp=skill_trees[i].charAt(j);
+            //System.out.println(que.peek());
+            if(arr[temp-65]){
+                if(que.peek()==temp){
+                    que.poll();
+                }
+                else
+                    break;
+            }
+
+        }
+        if(j==skill_trees[i].length())
+            answer++;
+    }
+    return answer;
+}
+}
